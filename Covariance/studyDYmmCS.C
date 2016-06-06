e96f2bd1ed1c5322bc0f5abc6baea909a27601da
Andrius Juodagalvis (supercomp) <andjuo@hotmail.com>
2016-06-06 08:05:51 +0300

    studyDYmmCS.C allows _varFSRRes_Poisson; embeds uncertainty from covariance; slim version of output file

--- Covariance/studyDYmmCS.C
+++ Covariance/studyDYmmCS.C
@@ -71,8 +71,25 @@ void work(MuonCrossSection_t &muCS, TVaried_t var, int nSample, int doSave)
   TCanvas *cx= plotCovCorr(h2Cov,"ccov",&h2Corr);
   if (!cx) std::cout << "cx is null\n";
 
+  TH1D *h1uncFromCov= uncFromCov(h2Cov);
+  TH1D *h1relUncFromCov= uncFromCov(h2Cov,muCS.h1CS());
+
+  if (!doSave) {
+    TH1D *h1CSUnc= errorAsCentral(muCS.h1CS(),0);
+    TH1D *h1CSRelUnc= errorAsCentral(muCS.h1CS(),1);
+    histoStyle(h1CSUnc,kBlue,24);
+    histoStyle(h1CSRelUnc,kBlue,24);
+    plotHisto(h1uncFromCov,"cUncCmp",1,1,"hist","uncFromCov");
+    plotHistoSame(h1CSUnc,"cUncCmp","P","csUnc");
+
+    plotHisto(h1relUncFromCov,"cRelUncCmp",1,1,"hist","rel.unc.from.cov");
+    plotHistoSame(h1CSRelUnc,"cRelUncCmp","P","cs.rel.unc");
+  }
+
   if (doSave) {
     TString fname="cov_mumu_" + variedVarName(var) + Form("_%d.root",nSample);
+    if (doSave==2) fname.ReplaceAll(".root","_slim.root");
+    if (var==_varFSRRes_Poisson) fname.ReplaceAll(".root","-Poisson.root");
     TFile fout(fname,"RECREATE");
     if (!fout.IsOpen()) {
       std::cout << "file <" << fname << "> could not be created\n";
@@ -80,12 +97,14 @@ void work(MuonCrossSection_t &muCS, TVaried_t var, int nSample, int doSave)
     }
     std::vector<TString> dirs;
     std::vector<std::vector<TH1D*>*> hVs;
-    dirs.push_back("rnd_CSa");
-    hVs.push_back(&rndCSaVec);
-    dirs.push_back("rnd_CSb");
-    hVs.push_back(&rndCSbVec);
-    dirs.push_back("rnd_CStot");
-    hVs.push_back(&rndCSVec);
+    if (doSave!=2) {
+      dirs.push_back("rnd_CSa");
+      hVs.push_back(&rndCSaVec);
+      dirs.push_back("rnd_CSb");
+      hVs.push_back(&rndCSbVec);
+      dirs.push_back("rnd_CStot");
+      hVs.push_back(&rndCSVec);
+    }
     for (unsigned int iDir=0; iDir<dirs.size(); iDir++) {
       std::vector<TH1D*> *hV= hVs[iDir];
       if (hV->size()) {
@@ -97,17 +116,20 @@ void work(MuonCrossSection_t &muCS, TVaried_t var, int nSample, int doSave)
 	fout.cd("");
       }
     }
-    if (muCS.h1CS()) muCS.h1CS()->Write();
-    if (muCS.h1Theory()) muCS.h1Theory()->Write();
-    if (h1Avg) h1Avg->Write();
-    if (h2Cov) h2Cov->Write();
-    if (h2Corr) h2Corr->Write();
+    if (muCS.h1CS()) muCS.h1CS()->Write("xsec");
+    if (muCS.h1Theory()) muCS.h1Theory()->Write("xsec_KL");
+    if (h1Avg) h1Avg->Write("h1xsecAvg");
+    if (h2Cov) h2Cov->Write("h2Cov");
+    if (h2Corr) h2Corr->Write("h2Corr");
+    if (h1uncFromCov) h1uncFromCov->Write("h1uncFromCov");
+    if (h1relUncFromCov) h1relUncFromCov->Write("h1relUncFromCov");
     if (c) c->Write();
     if (cx) cx->Write();
     std::vector<TCanvas*> canvV;
     TString canvList="muCS_a muCS_b";
     canvList.Append(" cVaried" + muCS.csA().tag());
     canvList.Append(" cVaried" + muCS.csB().tag());
+    if (doSave==2) canvList="";
     std::cout << "canvList=" << canvList << "\n";
     if (findCanvases(canvList,canvV)) {
       for (unsigned int ic=0; ic<canvV.size(); ic++) {
