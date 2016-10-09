5ca22d0ff75e21514c9f156f0eebe595d3b9b6e3
Andrius Juodagalvis (supercomp) <andjuo@hotmail.com>
2016-10-09 11:38:29 +0300

    work13TeV.C improved graphic appearance

--- Covariance/work13TeV.C
+++ Covariance/work13TeV.C
@@ -20,6 +20,9 @@ int combineData(const TMatrixD &measEE, const TMatrixD &covEE,
 		const TMatrixD &measMM, const TMatrixD &covMM,
 		const TH1D *h1binning);
 
+void checkSplit(const BLUEResult_t &blue,
+		const TMatrixD &covEE, const TMatrixD &covMM);
+
 // -----------------------------------------------------------------------
 // -----------------------------------------------------------------------
 
@@ -80,19 +83,14 @@ void work13TeV()
 
   if (0) {
     TMatrixD cov100 = reduceCorrelations(eeCovTot,-2.);
-    TMatrixD cov050 = reduceCorrelations(cov100,0.666);
+    TMatrixD cov066 = reduceCorrelations(cov100,0.666);
     TMatrixD cov000 = reduceCorrelations(cov100,1.0);
-    TH2D *h2covOrig= convert2histo(eeCovTot,h1csEE,"h2covOrig","h2covOrig");
-    TH2D *h2cov100 = convert2histo(cov100,h1csEE,"h2cov100","h2cov100");
-    TH2D *h2cov050 = convert2histo(cov050,h1csEE,"h2cov050","h2cov050");
-    TH2D *h2cov000 = convert2histo(cov000,h1csEE,"h2cov000","h2cov000");
-    plotCovCorr(h2covOrig,"cCov orig");
-    plotCovCorr(h2cov100,"cCov100corr fake tripled");
-    plotCovCorr(h2cov050,"cCov050corr fake one third");
-    plotCovCorr(h2cov000,"cCov000corr fake no corr");
+    plotCovCorr(eeCovTot,h1csEE,"h2covOrig","cCov orig");
+    plotCovCorr(cov100,h1csEE,"h2cov100","cCov100corr fake tripled");
+    plotCovCorr(cov066,h1csEE,"h2cov066","cCov066corr fake one third");
+    plotCovCorr(cov000,h1csEE,"h2cov000","cCov000corr fake no corr");
     return;
   }
-
 
   if (!combineData(measEE,eeCovStat,measMM,mmCovStat, h1csEE)) {
     std::cout << "failed to combined data. stopping\n";
@@ -142,8 +140,13 @@ int combineData(const TMatrixD &measEE_inp, const TMatrixD &covEE_inp,
   TMatrixD measEE(measEE_inp), covEE(covEE_inp);
   TMatrixD measMM(measMM_inp), covMM(covMM_inp);
 
-  if (0) { measEE=measMM; covEE=covMM; }
-  else if (0) { measMM=measEE; covMM=covEE; }
+  //if (0) { measEE=measMM; covEE=covMM; }
+  //else if (0) { measMM=measEE; covMM=covEE; }
+
+  if (1) { // uncorrelated case
+    covEE= reduceCorrelations(covEE_inp,1.);
+    covMM= reduceCorrelations(covMM_inp,1.);
+  }
 
   TMatrixD covEM(TMatrixD::kZero,covEE);
   BLUEResult_t blue;
@@ -165,30 +168,30 @@ int combineData(const TMatrixD &measEE_inp, const TMatrixD &covEE_inp,
     std::cout << "file <" << fCovFName << "> saved\n";
   }
 
+  TString massStr= niceMassAxisLabel(2,"");
+  TString eemassStr= niceMassAxisLabel(0,"");
+  TString mmmassStr= niceMassAxisLabel(1,"");
+  TString sigmaStr= niceMassAxisLabel(2,"",1);
+  TString axisStr= ";" + massStr + ";" + sigmaStr;
+
   if (1) {
     TH1D *h1EEErr= createErrHisto("h1EEErr","EE unc",covEE,nXbins,xBins);
     TH1D *h1MMErr= createErrHisto("h1MMErr","MM unc",covMM,nXbins,xBins);
     TH1D *h1LLErr= createErrHisto("h1LLErr","Comb unc",*blue.getCov(),nXbins,xBins);
-    h1EEErr->SetLineColor(kBlue);
-    h1EEErr->SetMarkerColor(kBlue);
-    h1EEErr->SetMarkerStyle(24);
-    h1EEErr->SetMarkerSize(0.5);
-    h1LLErr->SetLineColor(kRed);
-    h1LLErr->SetMarkerColor(kRed);
-    h1MMErr->SetMarkerStyle(20);
-    h1MMErr->SetMarkerSize(0.5);
-    h1EEErr->SetTitle("uncertainties");
+    histoStyle(h1EEErr,kBlue,24,1,0.7);
+    histoStyle(h1LLErr,kRed,7,2,1.);
+    histoStyle(h1MMErr,kBlack,20,1,0.7);
+    h1EEErr->SetTitle("uncertainties;"+massStr+";d"+sigmaStr);
+    h1EEErr->GetYaxis()->SetTitleOffset(1.5);
     int setLogX= (h1EEErr->GetNbinsX()<50) ? 1:0;
-    plotHisto(h1EEErr,"cErr",setLogX,1,"LP","DY#rightarrowee");
+    logAxis(h1EEErr);
+    h1EEErr->SetStats(0);
+    TCanvas *cErr=plotHisto(h1EEErr,"cErr",setLogX,1,"LP","DY#rightarrowee");
+    cErr->SetLeftMargin(0.15); gPad->Modified(); cErr->Update();
+    moveLegend(cErr,0.05,0.);
+    cErr->SetGrid(1,1);
     plotHistoSame(h1MMErr,"cErr","LP","DY#rightarrow#mu#mu");
     plotHistoSame(h1LLErr,"cErr","LP","combined");
-    //TCanvas *cErr= new TCanvas("cErr","cErr",600,600);
-    //if (h1EEErr->GetNbinsX()<50) cErr->SetLogx();
-    //cErr->SetLogy();
-    //h1EEErr->Draw("LPE1");
-    //h1LLErr->Draw("LPE1 same");
-    //h1MMErr->Draw("LPE1 same");
-    //cErr->Update();
     //return 1;
   }
 
@@ -200,95 +203,160 @@ int combineData(const TMatrixD &measEE_inp, const TMatrixD &covEE_inp,
     TGraphErrors *grLL= createGraph("combined measurement",
 				    *blue.getEst(),*blue.getCov(),
 				    nXbins,xBins, 0);
-
+    grLL->SetTitle("combined measurement" + axisStr);
+    grLL->GetYaxis()->SetTitleOffset(1.3);
     grEE->SetLineColor(kBlue);
     grEE->SetMarkerColor(kBlue);
     grLL->SetLineColor(kRed);
     grLL->SetMarkerColor(kRed);
 
-    grEE->SetMarkerStyle(7);
-    grMM->SetMarkerStyle(7);
+    graphStyle(grEE,kBlue, 20, 1,0.6);
+    graphStyle(grMM,kBlack,24, 1,0.6);
+    graphStyle(grLL,kRed, 5, 1, 1.);
+    //grEE->SetMarkerStyle(7);
+    //grMM->SetMarkerStyle(24);
 
     //grLL->GetXaxis()->SetRangeUser(10,50);
     //grLL->GetYaxis()->SetRangeUser(20,250);
 
     TCanvas *cMeas= new TCanvas("cMeas","cMeas", 600,600);
-    if (grLL->GetN()<50) {
+    cMeas->SetGrid(1,1);
+    int setLogXY= (grLL->GetN()<50) ? 1:0;
+    if (setLogXY) {
       cMeas->SetLogx();
       cMeas->SetLogy();
     }
+    logAxis(grLL,1+0*2);
     grLL->Draw("ALPE1");
     grEE->Draw("PE1 same");
     grMM->Draw("PE1 same");
     cMeas->Update();
 
-    grEE->Print("range");
-    grMM->Print("range");
-    grLL->Print("range");
+    //grEE->Print("range");
+    //grMM->Print("range");
+    //grLL->Print("range");
+
+    if (1) {
+      TGraphErrors *grLL_line= new TGraphErrors(*grLL);
+      grLL_line->SetLineStyle(3);
+
+      const int nDivs=5;
+      const double divX[nDivs][2] = { { 15., 60.},
+				      { 60., 102.},
+				      { 100., 200.},
+				      { 200., 600.},
+				      { 500., 3000.} };
+      const double divY[nDivs][2] = { { 3., 300. }, // 10-60
+				      { 3., 300. }, // 60-100
+				      { 2e-2, 10.}, // 100-200
+				      { 1e-4, 0.1}, // 200-600
+				      { 1e-9,1e-3} }; // >600
+      for (int iD=0; iD<nDivs; iD++) {
+	TString rangeStr=Form("%d_%d",int(divX[iD][0]),int(divX[iD][1]));
+	TString titleStr="combined measurement: " + rangeStr + axisStr;
+	TH2D *h2frame= new TH2D("h2frame" + rangeStr,titleStr,
+				100,divX[iD][0],divX[iD][1],
+				100,divY[iD][0],divY[iD][1]);
+	h2frame->SetDirectory(0);
+	h2frame->SetStats(0);
+	TString cName="cMeas_" + rangeStr;
+	TCanvas *cMeasD= new TCanvas(cName,cName,600,600);
+	cMeasD->SetGrid(1,1);
+	cMeasD->SetLogy();
+	logAxis(h2frame);
+	if (divY[iD][0]<1e-5) h2frame->GetYaxis()->SetNoExponent(0);
+	if (divX[iD][0]>490) {
+	  cMeasD->SetLeftMargin(0.15);
+	  h2frame->GetYaxis()->SetTitleOffset(2.1);
+	}
+	else {
+	  cMeasD->SetLeftMargin(0.11);
+	  h2frame->GetYaxis()->SetTitleOffset(1.4);
+	}
+	h2frame->Draw();
+	grLL_line->Draw("L");
+	grLL->Draw("PE1 same");
+	grEE->Draw("PE1 same");
+	grMM->Draw("PE1 same");
+	cMeasD->Update();
+      }
+    }
+
+
     //return 1;
   }
 
   if (0) {
     // check split
-    TMatrixD measCovEE(blue.measCovMatrix(covEE,1));
-    TMatrixD measCovMM(blue.measCovMatrix(covMM,0));
-    if (0) {
-      TMatrixD measCovTot(measCovEE, TMatrixD::kPlus, measCovMM);
-      TMatrixD chkDiff(measCovTot, TMatrixD::kMinus, *blue.getInpCov());
-      std::cout << "check difference: "; chkDiff.Print();
-    }
-
-    TMatrixD measCorrEE( covToCorr(measCovEE) );
-    TMatrixD measCorrMM( covToCorr(measCovMM) );
-
-    TCanvas *cSplit= new TCanvas("cSplit","cSplit",1200,600);
-    cSplit->Divide(2,1);
-    cSplit->cd(1);
-    measCorrEE.Draw("COLZ");
-    cSplit->cd(2);
-    measCorrMM.Draw("COLZ");
-    cSplit->Update();
-
-    if (1) {
-      TMatrixD corrEE( covToCorr(covEE) );
-      TMatrixD measCovEM(blue.measCovMatrix(corrEE,2));
-      if (0) { // to have something on a diagonal
-	measCovEM += removeNaNs(measCorrEE);
-	measCovEM += removeNaNs(measCorrMM);
-      }
-      TCanvas *cEMCovChk= new TCanvas("cEMCovChk","cEMCovChk",600,600);
-      measCovEM.Draw("COLZ");
-      cEMCovChk->Update();
-    }
-
+    checkSplit(blue,covEE,covMM);
     return 1;
-  } // check split
+  }
 
   TMatrixD covEEFinal( blue.contributedCov(blue.measCovMatrix(covEE,1)) );
   TMatrixD covMMFinal( blue.contributedCov(blue.measCovMatrix(covMM,0)) );
   TMatrixD covEMFinal( blue.contributedCov(blue.measCovMatrix(covEM,2)) );
-
-  TCanvas *cFinCov= new TCanvas("cFinCov","cFinCov",600,600);
   TMatrixD finalCov(*blue.getCov());
-  finalCov.Draw("COLZ");
-  cFinCov->Update();
 
-  TCanvas *cFinCorr=new TCanvas("cFinCorr","cFinCorr",600,600);
-  TMatrixD finalCorr(*blue.getCorr());
-  finalCorr.Draw("COLZ");
-  cFinCorr->Update();
+  h1binning->GetXaxis()->SetTitle(massStr);
+  TCanvas *cFinCov= plotCovCorr(finalCov,h1binning,"h2finCov","cFinCov",
+				NULL,NULL,1,1.8);
+  cFinCov->SetGrid(1,1);
 
-  TCanvas *cCorrPartEE= new TCanvas("cCorrPartEE","cCorrPartEE",600,600);
-  TMatrixD eeCorrPart(covToCorrPartial(covEEFinal,finalCov));
-  eeCorrPart.Draw("COLZ");
-  cCorrPartEE->Update();
+  h1binning->GetXaxis()->SetTitle(eemassStr);
+  TH2D* h2eeCorrPart= convert2histo(covToCorrPartial(covEEFinal,finalCov),
+				    h1binning,"h2eeCorrPart","h2eeCorrPart");
+  logAxis(h2eeCorrPart);
+  TCanvas *cCorrPartEE= plotHisto(h2eeCorrPart,"cCorrPartEE",1,1,1.8,1.);
+  cCorrPartEE->SetGrid(1,1);
 
-  TCanvas *cCorrPartMM= new TCanvas("cCorrPartMM","cCorrPartMM",600,600);
-  TMatrixD mmCorrPart(covToCorrPartial(covMMFinal,finalCov));
-  mmCorrPart.Draw("COLZ");
-  cCorrPartMM->Update();
+  h1binning->GetXaxis()->SetTitle(mmmassStr);
+  TH2D* h2mmCorrPart= convert2histo(covToCorrPartial(covMMFinal,finalCov),
+				    h1binning,"h2mmCorrPart","h2mmCorrPart");
+  logAxis(h2mmCorrPart);
+  TCanvas *cCorrPartMM= plotHisto(h2mmCorrPart,"cCorrPartMM",1,1,1.8,1.);
+  cCorrPartMM->SetGrid(1,1);
 
   return 1;
+}
+
+// -----------------------------------------------------------------------
+
+// check BLUEResult object
+
+void checkSplit(const BLUEResult_t &blue,
+		const TMatrixD &covEE, const TMatrixD &covMM)
+{
+  TMatrixD measCovEE(blue.measCovMatrix(covEE,1));
+  TMatrixD measCovMM(blue.measCovMatrix(covMM,0));
+  if (0) {
+    TMatrixD measCovTot(measCovEE, TMatrixD::kPlus, measCovMM);
+    TMatrixD chkDiff(measCovTot, TMatrixD::kMinus, *blue.getInpCov());
+      std::cout << "check difference: "; chkDiff.Print();
+  }
+
+  TMatrixD measCorrEE( covToCorr(measCovEE) );
+  TMatrixD measCorrMM( covToCorr(measCovMM) );
+
+  TCanvas *cSplit= new TCanvas("cSplit","cSplit",1200,600);
+  cSplit->Divide(2,1);
+  cSplit->cd(1);
+  measCorrEE.Draw("COLZ");
+  cSplit->cd(2);
+  measCorrMM.Draw("COLZ");
+  cSplit->Update();
+
+  if (1) {
+    TMatrixD corrEE( covToCorr(covEE) );
+    TMatrixD measCovEM(blue.measCovMatrix(corrEE,2));
+    if (0) { // to have something on a diagonal
+      measCovEM += removeNaNs(measCorrEE);
+      measCovEM += removeNaNs(measCorrMM);
+    }
+    TCanvas *cEMCovChk= new TCanvas("cEMCovChk","cEMCovChk",600,600);
+    measCovEM.Draw("COLZ");
+    cEMCovChk->Update();
+  }
+
 }
 
 // -----------------------------------------------------------------------
