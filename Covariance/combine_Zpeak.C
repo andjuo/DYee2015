c2871c9bfd7f08ed8f8fca24121c93af204b09fb
Andrius Juodagalvis (supercomp) <andjuo@hotmail.com>
2017-05-27 14:36:00 +0300

    combine_Zpeak.C: updated to newerst Z-peak cross-section values

--- Covariance/combine_Zpeak.C
+++ Covariance/combine_Zpeak.C
@@ -1,11 +1,12 @@
 #include "inputs.h"
 #include "Blue.h"
 
-void combine_Zpeak(int theCase=1)
+void combine_Zpeak(int theCase=1, int subVer=0)
 {
   double sigmaZee=0, sigmaZmm=0;
   std::vector<double> dSigmaEE,dSigmaMM,dSigmaEM;
   std::vector<TString> labelV;
+  double dLumiRel=2;
 
   if (valueEquals(theCase,"1 -1 10 11")) {
     // SMP-15-004-pas
@@ -76,7 +77,7 @@ void combine_Zpeak(int theCase=1)
       addToVector(labelV,"lumi");
     }
   }
-  else if (valueEquals(theCase,"13 1313 131313 130 -13")) {
+  else if ((subVer==0) && valueEquals(theCase,"13 1313 131313 130 -13")) {
     // DY 13TeV
     sigmaZee= 1861.0;
     addToVector(dSigmaEE,2,2.,1000.);
@@ -99,6 +100,36 @@ void combine_Zpeak(int theCase=1)
     if ((theCase==1313) || (theCase==131313)) {
       addToVector(dSigmaEE,1,round(sigmaZee*0.027));
       addToVector(dSigmaMM,1,round(sigmaZmm*0.027));
+      addToVector(dSigmaEM,1,sqrt(dSigmaEE.back()*dSigmaMM.back()));
+      addToVector(labelV,"lumi");
+    }
+  }
+  else if ((subVer==1) && valueEquals(theCase,"13 1313 131313 130 -13")) {
+    // DY 13TeV
+    // ee: 1949 +- 2 (stat) +- 28 (exp.syst) +- 32 (theo) +- 45 (lumi) pb
+    // mm: 1907 +- 1 (Stat) +- 14 (exp.syst) +- 29 (theo) +- 44 (lumi) pb
+    dLumiRel=0.023;
+    sigmaZee= 1949.0;
+    addToVector(dSigmaEE,2,2.,28.);
+    sigmaZmm= 1907.0;
+    addToVector(dSigmaMM,2,1.,14.);
+    addToVector(dSigmaEM,2,0.,0.);
+    addToVector(labelV,"stat expSyst");
+    if (theCase==-13) {
+      dSigmaEE.clear(); addToVector(dSigmaEE,1,2.);
+      dSigmaMM.clear(); addToVector(dSigmaMM,1,1.);
+      dSigmaEM.clear(); addToVector(dSigmaEM,1,0.);
+      labelV.clear(); addToVector(labelV,"stat");
+    }
+    if ((theCase==130) || (theCase==131313)) {
+      addToVector(dSigmaEE,1,32.);
+      addToVector(dSigmaMM,1,29.);
+      addToVector(dSigmaEM,1,sqrt(32.*29.));
+      addToVector(labelV,"th.syst");
+    }
+    if ((theCase==1313) || (theCase==131313)) {
+      addToVector(dSigmaEE,1,round(sigmaZee*dLumiRel));
+      addToVector(dSigmaMM,1,round(sigmaZmm*dLumiRel));
       addToVector(dSigmaEM,1,sqrt(dSigmaEE.back()*dSigmaMM.back()));
       addToVector(labelV,"lumi");
     }
