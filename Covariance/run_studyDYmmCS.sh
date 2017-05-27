09b2edf356b121d2b65bbac91573575d497cc3ac
Andrius Juodagalvis (supercomp) <andjuo@hotmail.com>
2017-05-27 14:34:03 +0300

    run_studyDYmmCS.sh: generalized for mm and ee

--- Covariance/run_studyDYmmCS.sh
+++ Covariance/run_studyDYmmCS.sh
@@ -8,6 +8,21 @@ vars="${vars} _varRhoFile"
 nSample=2000
 doSave=1
 
+code="studyDYxxCS.C"
+if [ ${#1} -gt 0 ] ; then
+    if [ "$1" == "ee" ] ; then
+	code="studyDYeeCS.C"
+	echo -e "\n\tElectron code\n"
+    elif [ "$1" == "mm" ] ; then
+	code="studyDYmmCS.C"
+	echo -e "\n\tMuon code\n"
+    fi
+else
+    echo -e "\nUse:./run_studyDYmmCS.sh {ee|mm}"
+    exit 1
+fi
+
 for v in ${vars} ; do
-    root -l -q -b studyDYmmCS.C+"(${v},${nSample},${doSave})"
+    echo "code=${code}, v=${v}"
+    root -l -q -b ${code}+"(${v},${nSample},${doSave})"
 done
